276a9b03d00b9be17f0491b1211a3021837e62e0
GrangerVMV <javitrunk@hotmail.com>
2022-09-13 17:57:01 +0200

    Tercer método: suma números por valor con return

--- Program.cs
+++ Program.cs
@@ -19,6 +19,9 @@ namespace CursoCPractica2_Metodos
             MensajeEnPantalla();
 
             SumaNumerosVal(7, 9); //Segundo método: Suma números por valor
+
+            Console.WriteLine($"El resultado de la suma con return es {sumaNumeros(5, 4)}"); // Tercer método: suma números y devuelve un valor
+
         }
 
         //Segundo método: Suma números por valor
@@ -26,8 +29,17 @@ namespace CursoCPractica2_Metodos
         static void SumaNumerosVal(int num1, int num2)
         {
             Console.WriteLine($"El resultado de la suma es {num1+num2}");
-            
+                                 
         }
+
+        //Tercer método: Suma números con return
+        static int sumaNumeros(int num1, int num2)
+        {
+            return (num1 + num2);
+        }
+
+
+
 
     }
 }
