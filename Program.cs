5bac7d274f8329e6d32c0692b9ee340a09ed561b
GrangerVMV <javitrunk@hotmail.com>
2022-09-14 13:18:19 +0200

    Octavo método: crea varios métodos con el mismo nombre (sobrecargas)

--- Program.cs
+++ Program.cs
@@ -25,6 +25,8 @@ namespace CursoCPractica2_Metodos
             Console.WriteLine($"El resultado de la división con return es {divideNumeros(8,6)}"); // Llamada al Cuarto método: divide números con return
 
             Console.WriteLine($"El resultado de la multiplicación con operador flecha es {multiplicaNumeros(7,3)}");// Llamada al Quinto método: Multiplica números con operador flecha en vez de return
+
+            Console.WriteLine($"El resultado de la suma con sobrecargas es {Suma(1, 2, 4, 8)}"); // Llamada a Octavo método: Crea varios métodos con elmismo nombre (sobrecargas)
         }
 
         // ****************** Métodos ******************
@@ -62,6 +64,15 @@ namespace CursoCPractica2_Metodos
         {
             Console.WriteLine($"La resta de los campos de clase es {campoClase1 - campoClase2}"); // Si no se definen a nivel de clase (global) las mismas variables no se pueden usar en sistintos métodos
         }
+
+        // Octavo método: Crea varios métodos con elmismo nombre (sobrecargas)
+        //Para crear varios métodos con distinto nombre deben tener distinto número o distinto tipo de parámetros
+
+        static int Suma(int sumando1, int sumando2) => sumando1 + sumando2;
+        static int Suma(int numero1, int numero2, int numero3, int numero4) => numero1 + numero2 + numero3 + numero4;
+        static int Suma(int numero1, double numero2) => numero1;
+        
+
      int campoClase1 = 10; // Las variables globales (campos de clase) se pueden usar primero y definir después -> no sigue el flujo de ejecucuón
      int campoClase2 = 4;
 
