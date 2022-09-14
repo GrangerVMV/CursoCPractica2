c580539d2bcad907767cd1f878ae50255fc6cb7c
GrangerVMV <javitrunk@hotmail.com>
2022-09-14 12:25:22 +0200

    Sexto y séptimo método: utiliza variables de ámbito local (campos de clase)

--- Program.cs
+++ Program.cs
@@ -13,19 +13,22 @@ namespace CursoCPractica2_Metodos
         static void Main(string[] args) //Lee primero el método y después el Console.WriteLine
         {
             //Console.WriteLine("Hello World!");
-            MensajeEnPantalla();
+            MensajeEnPantalla(); //Llamada al Primer método: Imprime un mensaje en pantalla
             Console.WriteLine("Mensaje desde el Main");
             MensajeEnPantalla();// Para entender el flujo del programa método-Console.WriteLine-metodo-metodo
             MensajeEnPantalla();
 
-            SumaNumerosVal(7, 9); //Segundo método: Suma números por valor
+            SumaNumerosVal(7, 9); //Llamada al Segundo método: Suma números por valor
 
-            Console.WriteLine($"El resultado de la suma con return es {sumaNumeros(5, 4)}"); // Tercer método: suma números y devuelve un valor
+            Console.WriteLine($"El resultado de la suma con return es {sumaNumeros(5, 4)}"); // Llamada al Tercer método: suma números y devuelve un valor
 
-            Console.WriteLine($"El resultado de la división con return es {divideNumeros(8,6)}"); // Cuarto método: divide números con return
+            Console.WriteLine($"El resultado de la división con return es {divideNumeros(8,6)}"); // Llamada al Cuarto método: divide números con return
 
-            Console.WriteLine($"El resultado de la multiplicación con operador flecha es {multiplicaNumeros(7,3)}");
+            Console.WriteLine($"El resultado de la multiplicación con operador flecha es {multiplicaNumeros(7,3)}");// Llamada al Quinto método: Multiplica números con operador flecha en vez de return
         }
+
+        // ****************** Métodos ******************
+
 
         //Segundo método: Suma números por valor
 
@@ -49,6 +52,20 @@ namespace CursoCPractica2_Metodos
 
         // Quinto método: Multiplica números con operador flecha en vez de return
         static int multiplicaNumeros(int num1, int num2) => num1 * num2;
+
+        // Sexto y Séptimo método: Utiliza variables de ambito global (campos de clase)    
+        void sumaCampoClase()
+        {
+            Console.WriteLine($"La suma de los campos de clase es {campoClase1 + campoClase2}"); // Si no se definen a nivel de clase (global) las mismas variables no se pueden usar en sistintos métodos
+        }
+        void restaCampoClase()
+        {
+            Console.WriteLine($"La resta de los campos de clase es {campoClase1 - campoClase2}"); // Si no se definen a nivel de clase (global) las mismas variables no se pueden usar en sistintos métodos
+        }
+     int campoClase1 = 10; // Las variables globales (campos de clase) se pueden usar primero y definir después -> no sigue el flujo de ejecucuón
+     int campoClase2 = 4;
+
+
 
     }
 }
